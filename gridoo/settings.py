e60642d7bb7ff2b6341c3e4ffa82b694252c0f73
Harshit <harshitverma84476@gmail.com>
2026-05-01 02:41:39 +0530

    Add Railway deployment config

--- gridoo/settings.py
+++ gridoo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,7 @@ STATIC_URL = 'static/'
 TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+ALLOWED_HOSTS = ['*']
+DEBUG = False
+STATIC_ROOT = BASE_DIR / 'staticfiles'
