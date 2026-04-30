508965de06bec70f19bd9c496438a679172054b9
Harshit <harshitverma84476@gmail.com>
2026-05-01 02:58:33 +0530

    Fix static files with whitenoise

--- gridoo/settings.py
+++ gridoo/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,3 +138,4 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 ALLOWED_HOSTS = ['*']
 DEBUG = False
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
